3dfd71fc1578eaf0b256620dc837845c20ef03b9
happytudouni <45924665+happytudouni@users.noreply.github.com>
2022-05-06 16:55:04 +0800

    Update RunMe_example.m

--- pl_pps/SourceSpaceFC/Programs/RunMe_example.m
+++ pl_pps/SourceSpaceFC/Programs/RunMe_example.m
@@ -3,6 +3,7 @@ clc
 
 %% Set environment
 restoredefaultpath;
+ 
 
 dir_matlab = fullfile('..','..','..','..','anwenkang@gmail.com - Google Drive','My Drive','Research_MATLAB/');
 addpath(fullfile(dir_matlab,'eeglab2019_0/'));
